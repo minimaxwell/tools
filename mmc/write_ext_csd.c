6f4fdec4508c5597b00e7437ee8906d3dca7d15e
Chevallier Maxime <maxime.chevallier@openwide.fr>
2016-01-14 08:45:04 +0100

    Small license fix

--- mmc/write_ext_csd.c
+++ mmc/write_ext_csd.c
@@ -1,4 +1,7 @@
-/*Copyright (c) 2016 Maxime Chevallier
+/* 
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016 Maxime Chevallier
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
