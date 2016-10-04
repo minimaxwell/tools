55319a1b6d11a95169012cf1bcf008ce75037e7d
Maxime Chevallier <maxime.chevallier@smile.fr>
2016-10-04 15:20:36 +0200

    [ble] Ble logger, init commit

---
+++ ble/bt_log/bt_log.c
@@ -0,0 +1,8 @@
+#include "monitor.h"
+#include "stdio.h"
+
+int main( int argc, char **argv ) {
+
+	start_scan();
+	return 0;
+}
---
+++ ble/bt_log/monitor.h
@@ -0,0 +1,5 @@
+#ifndef __MONITOR_H__
+#define __MONITOR_H__
+
+void start_scan();
+#endif
